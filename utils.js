d0583f0731a877a17a422920e77c352174493a2f
Melchiah <melchiahmauck@gmail.com>
2022-08-02 22:30:44 -0400

    Made navbar and experiences buttons.

--- utils.js
+++ utils.js
@@ -1,3 +1,9 @@
+// Opens the given url in a new tab
 function openUrlInNewTab(url) {
   window.open(url, "_blank");
+}
+
+// Opens the given page
+function goToPage(url) {
+  window.location.href = url;
 }
